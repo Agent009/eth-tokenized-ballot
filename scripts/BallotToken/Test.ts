f53a904c67bebeef9e1197042daef4c82b295b3a
Mohammad Amir <amir@connextar.com>
2024-11-12 09:58:18 +0000

    Script tidy-up.

--- scripts/BallotToken/Test.ts
+++ scripts/BallotToken/Test.ts
@@ -26,92 +26,49 @@ async function main(): Promise<void> {
   const acc2Account = acc2!.account;
   const acc2Address = acc2Account.address;
   const contract = await viem.deployContract(CONTRACT_NAME);
-  console.log(`${MSG_PREFIX} -> Token contract deployed at ${contract.address}\n`);
+  let blockNo = await publicClient.getBlockNumber();
+  console.log(`${MSG_PREFIX} -> Token contract deployed at ${contract.address}`, "block", blockNo);
 
   // Minting some tokens
   const mintTx = await contract.write.mint([acc1Address, MINT_VALUE]);
   await publicClient.waitForTransactionReceipt({ hash: mintTx });
-  console.log(
-    `${MSG_PREFIX} -> Minted ${MINT_VALUE.toString()} decimal units to account ${
-      acc1Address
-    }\n`
-  );
+  blockNo = await publicClient.getBlockNumber();
+  console.log(`${MSG_PREFIX} -> Minted ${MINT_VALUE.toString()} decimal units to acc1`, "block", blockNo);
   const balanceBN = await contract.read.balanceOf([acc1Address]);
-  console.log(
-    `${MSG_PREFIX} -> Account ${
-      acc1Address
-    } has ${balanceBN.toString()} decimal units of BallotToken\n`
-  );
+  console.log(`${MSG_PREFIX} -> acc1 has ${balanceBN.toString()} decimal units of BallotToken`);
 
   // Checking vote power
   const votes = await contract.read.getVotes([acc1Address]);
-  console.log(
-    `${MSG_PREFIX} -> Account ${
-      acc1Address
-    } has ${votes.toString()} units of voting power before self delegating\n`
-  );
+  console.log(`${MSG_PREFIX} -> acc1 has ${votes.toString()} units of voting power before self delegating`);
 
   // Self delegation transaction
-  const delegateTx = await contract.write.delegate([acc1Address], {
-    account: acc1Account,
-  });
+  const delegateTx = await contract.write.delegate([acc1Address], { account: acc1Account });
   await publicClient.waitForTransactionReceipt({ hash: delegateTx });
+  blockNo = await publicClient.getBlockNumber();
   const votesAfter = await contract.read.getVotes([acc1Address]);
-  console.log(
-    `${MSG_PREFIX} -> Account ${
-      acc1Address
-    } has ${votesAfter.toString()} units of voting power after self delegating\n`
-  );
+  console.log(`${MSG_PREFIX} -> acc1 has ${votesAfter.toString()} units of voting power after self delegating`, "block", blockNo);
 
   // Experimenting a token transfer
-  const transferTx = await contract.write.transfer(
-    [acc2Address, MINT_VALUE / 2n],
-    {
-      account: acc1Account,
-    }
-  );
+  const transferTx = await contract.write.transfer([acc2Address, MINT_VALUE / 2n], { account: acc1Account });
   await publicClient.waitForTransactionReceipt({ hash: transferTx });
-  const votes1AfterTransfer = await contract.read.getVotes([
-    acc1Address,
-  ]);
-  console.log(
-    `${MSG_PREFIX} -> Account ${
-      acc1Address
-    } has ${votes1AfterTransfer.toString()} units of voting power after transferring\n`
-  );
-  const votes2AfterTransfer = await contract.read.getVotes([
-    acc2Address,
-  ]);
-  console.log(
-    `${MSG_PREFIX} -> Account ${
-      acc2Address
-    } has ${votes2AfterTransfer.toString()} units of voting power after receiving a transfer\n`
-  );
+  blockNo = await publicClient.getBlockNumber();
+  const votes1AfterTransfer = await contract.read.getVotes([acc1Address]);
+  console.log(`${MSG_PREFIX} -> acc1 has ${votes1AfterTransfer.toString()} units of voting power after transferring`, "block", blockNo);
+  const votes2AfterTransfer = await contract.read.getVotes([acc2Address]);
+  console.log(`${MSG_PREFIX} -> acc2 has ${votes2AfterTransfer.toString()} units of voting power after receiving a transfer`);
 
   // Self delegation transaction
-  const delegateTx2 = await contract.write.delegate([acc2Address], {
-    account: acc2Account,
-  });
+  const delegateTx2 = await contract.write.delegate([acc2Address], { account: acc2Account });
   await publicClient.waitForTransactionReceipt({ hash: delegateTx2 });
+  blockNo = await publicClient.getBlockNumber();
   const votesAfter2 = await contract.read.getVotes([acc2Address]);
-  console.log(
-    `${MSG_PREFIX} -> Account ${
-      acc2Address
-    } has ${votesAfter2.toString()} units of voting power after self delegating\n`
-  );
+  console.log(`${MSG_PREFIX} -> acc2 has ${votesAfter2.toString()} units of voting power after self delegating`, "block", blockNo, "\n");
 
   // Checking past votes
   const lastBlockNumber = await publicClient.getBlockNumber();
   for (let index = lastBlockNumber - 1n; index > 0n; index--) {
-    const pastVotes = await contract.read.getPastVotes([
-      acc1Address,
-      index,
-    ]);
-    console.log(
-      `${MSG_PREFIX} -> Account ${
-        acc1Address
-      } had ${pastVotes.toString()} units of voting power at block ${index}\n`
-    );
+    const pastVotes = await contract.read.getPastVotes([acc1Address, index]);
+    console.log(`${MSG_PREFIX} -> acc1 had ${pastVotes.toString()} units of voting power at block ${index}`);
   }
 }
 
